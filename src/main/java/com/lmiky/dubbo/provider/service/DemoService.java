bbfe030c3a3ee957384f824302f09eb67049243c
linminqin <flylmiky@gmail.com>
2015-06-12 15:30:20 +0800

    commit

---
+++ src/main/java/com/lmiky/dubbo/provider/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.lmiky.dubbo.provider.service;
+
+/**
+ * 测试
+ * @author lmiky
+ * @date 2015年6月12日 上午11:13:32
+ */
+public interface DemoService {
+	
+	/**
+	 * say hello
+	 * @author lmiky
+	 * @date 2015年6月12日 上午11:29:54
+	 * @param name
+	 * @return
+	 */
+	public String sayHello(String name);
+	
+}
