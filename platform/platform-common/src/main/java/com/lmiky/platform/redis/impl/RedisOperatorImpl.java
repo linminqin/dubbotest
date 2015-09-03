f7fc79ccffe0844ceb9b64b418f98e67d56d8185
linminqin <flylmiky@gmail.com>
2015-09-03 21:55:20 +0800

    commit

--- platform/platform-common/src/main/java/com/lmiky/platform/redis/impl/RedisOperatorImpl.java
+++ platform/platform-common/src/main/java/com/lmiky/platform/redis/impl/RedisOperatorImpl.java
@@ -1,63 +1,71 @@
 package com.lmiky.platform.redis.impl;
 
-import java.util.Map;
+import com.lmiky.platform.redis.RedisOperator;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
 
-import com.lmiky.platform.redis.RedisOperator;
+import java.util.Map;
 
 /**
  * Redis操作工具
+ *
  * @author lmiky
  * @date 2015年5月12日 下午1:01:41
  */
-@Component("redisOperator")
+// @Component("redisOperator")
 public class RedisOperatorImpl implements RedisOperator {
-	private RedisTemplate<String, Object> redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
 
-	/* (non-Javadoc)
-	 * @see com.lmiky.platform.redis.RedisOperator#hset(java.lang.String, java.lang.String, java.lang.Object)
-	 */
-	public void hset(String key, String field, Object value) {
-		redisTemplate.boundHashOps(key).put(field, value);
-	}
+    /*
+     * (non-Javadoc)
+     * @see com.lmiky.platform.redis.RedisOperator#hset(java.lang.String, java.lang.String, java.lang.Object)
+     */
+    @Override
+    public void hset(String key, String field, Object value) {
+        redisTemplate.boundHashOps(key).put(field, value);
+    }
 
-	/* (non-Javadoc)
-	 * @see com.lmiky.platform.redis.RedisOperator#hget(java.lang.String, java.lang.String)
-	 */
-	public Object hget(String key, String field) {
-		return redisTemplate.boundHashOps(key).get(field);
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.lmiky.platform.redis.RedisOperator#hgetAll(java.lang.String)
-	 */
-	public Map<Object, Object> hgetAll(String key) {
-		return redisTemplate.boundHashOps(key).entries();
-	}
-	
-	/* (non-Javadoc)
-	 * @see com.lmiky.platform.redis.RedisOperator#hdel(java.lang.String, java.lang.String)
-	 */
-	public void hdel(String key, String field) {
-		redisTemplate.boundHashOps(key).delete(field);
-		
-	}
-	
-	/**
-	 * @return the redisTemplate
-	 */
-	public RedisTemplate<String, Object> getRedisTemplate() {
-		return redisTemplate;
-	}
+    /*
+     * (non-Javadoc)
+     * @see com.lmiky.platform.redis.RedisOperator#hget(java.lang.String, java.lang.String)
+     */
+    @Override
+    public Object hget(String key, String field) {
+        return redisTemplate.boundHashOps(key).get(field);
+    }
 
-	/**
-	 * @param redisTemplate the redisTemplate to set
-	 */
-	@Autowired
-	public void setRedisTemplate(RedisTemplate<String, Object> redisTemplate) {
-		this.redisTemplate = redisTemplate;
-	}
+    /*
+     * (non-Javadoc)
+     * @see com.lmiky.platform.redis.RedisOperator#hgetAll(java.lang.String)
+     */
+    @Override
+    public Map<Object, Object> hgetAll(String key) {
+        return redisTemplate.boundHashOps(key).entries();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see com.lmiky.platform.redis.RedisOperator#hdel(java.lang.String, java.lang.String)
+     */
+    @Override
+    public void hdel(String key, String field) {
+        redisTemplate.boundHashOps(key).delete(field);
+
+    }
+
+    /**
+     * @return the redisTemplate
+     */
+    public RedisTemplate<String, Object> getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    /**
+     * @param redisTemplate the redisTemplate to set
+     */
+    @Autowired
+    public void setRedisTemplate(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
 }
