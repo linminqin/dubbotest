6f21c2182495af2be1310f40359bc5452695a605
linminqin <flylmiky@gmail.com>
2015-09-09 22:56:36 +0800

    优化数据库读写分离，添加多读数据源支持

--- platform/platform-service-impl/src/main/java/com/lmiky/platform/database/datasource/DynamicDataSource.java
+++ platform/platform-service-impl/src/main/java/com/lmiky/platform/database/datasource/DynamicDataSource.java
@@ -1,5 +1,10 @@
 package com.lmiky.platform.database.datasource;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
@@ -10,13 +15,81 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
 
+    private Object writeDataSource; 
+    private List<Object> readDataSources;
+    private int readDataSourceSize = 0;
+    
+    private AtomicInteger readIndex = new AtomicInteger(0);
+    
+    /**
+     * 数据源键名
+     */
+    private static final String DATASOURCE_KEY_WRITE = "write";
+    private static final String DATASOURCE_KEY_READ = "read";
+    
+    /* (non-Javadoc)
+     * @see org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource#afterPropertiesSet()
+     */
+    @Override
+    public void afterPropertiesSet() {
+        if (this.writeDataSource == null) {
+            throw new IllegalArgumentException("Property 'writeDataSource' is required");
+        }
+        setDefaultTargetDataSource(writeDataSource);
+        Map<Object, Object> targetDataSources = new HashMap<>();
+        targetDataSources.put(DATASOURCE_KEY_WRITE, writeDataSource);
+        if (this.readDataSources == null) {
+            readDataSourceSize = 0;
+        } else {
+            for(int i=0; i<readDataSources.size(); i++) {
+                targetDataSources.put(DATASOURCE_KEY_READ + i, readDataSources.get(i));
+            }
+            readDataSourceSize = readDataSources.size();
+        }
+        setTargetDataSources(targetDataSources);
+        super.afterPropertiesSet();
+    }
+
     /*
      * (non-Javadoc)
      * @see org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource#determineCurrentLookupKey()
      */
     @Override
     protected Object determineCurrentLookupKey() {
-        return DynamicDataSourceHolder.getDataSouce();
+        if(DynamicDataSourceHolder.isChoiceNone() || DynamicDataSourceHolder.isChoiceWrite() || readDataSourceSize == 0) {
+            return DATASOURCE_KEY_WRITE;
+        }
+        int index = readIndex.incrementAndGet() % readDataSourceSize;
+        return DATASOURCE_KEY_READ + index;
     }
+
+    /**
+     * @return the writeDataSource
+     */
+    public Object getWriteDataSource() {
+        return writeDataSource;
+    }
+
+    /**
+     * @param writeDataSource the writeDataSource to set
+     */
+    public void setWriteDataSource(Object writeDataSource) {
+        this.writeDataSource = writeDataSource;
+    }
+
+    /**
+     * @return the readDataSources
+     */
+    public List<Object> getReadDataSources() {
+        return readDataSources;
+    }
+
+    /**
+     * @param readDataSources the readDataSources to set
+     */
+    public void setReadDataSources(List<Object> readDataSources) {
+        this.readDataSources = readDataSources;
+    }
+
 
 }
